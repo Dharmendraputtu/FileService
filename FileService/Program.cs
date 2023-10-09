1c08e1b5f8632766c78c05e76a6cce2b39841b80
Dharmendra Puttu <dharmendra.puttu@outlook.com>
2023-10-08 23:15:16 -0400

    Add project files.

---
+++ FileService/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            //Scheduler scheduler=new Scheduler();
+            //scheduler.ScheduleService();
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Scheduler()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ FileService/Scheduler.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+namespace FileService
+{
+    public partial class Scheduler : ServiceBase
+    {
+        public Scheduler()
+        {
+            InitializeComponent();
+        }
+
+       
+        protected override void OnStart(string[] args)
+        {
+            this.WriteToFile("Simple Service started {0}");
+            this.ScheduleService();
+        }
+
+        protected override void OnStop()
+        {
+            this.WriteToFile("Simple Service stopped {0}");
+            this.timer.Dispose();
+        }
+
+        private Timer timer;
+
+        public void ScheduleService()
+        {
+            try
+            {
+                timer = new Timer(new TimerCallback(SchedularCallback));
+                string mode = ConfigurationManager.AppSettings["Mode"].ToUpper();
+                this.WriteToFile("Simple Service Mode: " + mode + " {0}");
+
+                //Set the Default Time.
+                DateTime scheduledTime = DateTime.MinValue;
+
+                if (mode == "DAILY")
+                {
+                    //Get the Scheduled Time from AppSettings.
+                    scheduledTime = DateTime.Parse(ConfigurationManager.AppSettings["ScheduledTime"]);
+                    if (DateTime.Now > scheduledTime)
+                    {
+                        //If Scheduled Time is passed set Schedule for the next day.
+                        scheduledTime = scheduledTime.AddDays(1);
+                    }
+                }
+
+                if (mode.ToUpper() == "INTERVAL")
+                {
+                    //Get the Interval in Minutes from AppSettings.
+                    int intervalMinutes = Convert.ToInt32(ConfigurationManager.AppSettings["IntervalMinutes"]);
+
+                    //Set the Scheduled Time by adding the Interval to Current Time.
+                    scheduledTime = DateTime.Now.AddMinutes(intervalMinutes);
+                    if (DateTime.Now > scheduledTime)
+                    {
+                        //If Scheduled Time is passed set Schedule for the next Interval.
+                        scheduledTime = scheduledTime.AddMinutes(intervalMinutes);
+                    }
+                }
+
+                TimeSpan timeSpan = scheduledTime.Subtract(DateTime.Now);
+                string schedule = string.Format("{0} day(s) {1} hour(s) {2} minute(s) {3} seconds(s)", timeSpan.Days, timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+
+                this.WriteToFile("Simple Service scheduled to run after: " + schedule + " {0}");
+
+                //Get the difference in Minutes between the Scheduled and Current Time.
+                int dueTime = Convert.ToInt32(timeSpan.TotalMilliseconds);
+
+                //Change the Timer's Due Time.
+                timer.Change(dueTime, Timeout.Infinite);
+            }
+            catch (Exception ex)
+            {
+                WriteToFile("Simple Service Error on: {0} " + ex.Message + ex.StackTrace);
+
+                //Stop the Windows Service.
+                using (ServiceController serviceController = new ServiceController("SimpleService"))
+                {
+                    serviceController.Stop();
+                }
+            }
+        }
+
+        private void SchedularCallback(object e)
+        {
+            this.WriteToFile("Simple Service Log: {0}");
+            this.ScheduleService();
+        }
+
+        private void WriteToFile(string text)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(string.Format(text, DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+            //string path = "C:\\ServiceLog.txt";
+            //using (StreamWriter writer = new StreamWriter(path, true))
+            //{
+            //    sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+
+            //    writer.WriteLine(string.Format(text, DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+            //    writer.Close();
+            //}
+        }
+    }
+}
